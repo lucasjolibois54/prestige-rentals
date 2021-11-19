1deef6ccddad9cd5a6e5949dc18c042306f3b8b6
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-19 18:44:50 +0100

    Update SubHero.js

--- src/components/SubHero.js
+++ src/components/SubHero.js
@@ -6,18 +6,18 @@ function SubHero() {
     return (
       <div>
       <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
-         <div className="w-full xl:w-9/12 grid lg:justify-items-end">
-          <img src={phone2} className="pt-16 w-full xl:w-9/12" alt="Phone Mockup" />
+         <div className="w-full xl:w-9/12 grid lg:justify-items-center">
+          <img src={phone2} className="pt-16 w-full xl:w-10/12" alt="Phone Mockup" />
       </div>
-      <div className="flex text-center flex-col w-9/12 px-3 justify-center xl:w25/10 xl:text-left xl:pl-10">
-          <h3 className="text-lg pt-10 text-gray-900 mb-3 font-light">
+          <div className="flex text-center flex-col w-9/12 px-3 justify-center xl:w-8/12 xl:text-left ">      {/*  PL-10 */}
+          <h3 className="text-lg pt-10 text-gray-400 mb-3 font-light">
           Convenient Interaction
           </h3>
-          <h2 className="text-4xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans lg:mt-0 mt-4 text-dark-text py-0 mb-8">
+          <h2 className="text-4xl text-dark-blue sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans lg:mt-0 mt-4 text-dark-text py-0 mb-8">
           Modern App
               
           </h2>
-          <p className="text-base  md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+          <p className="text-base text-dark-blue md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-26">
           Don’t deny yourself the pleasure of driving the best premium cars from around the world here and now. Don’t deny yourself the pleasure of driving the best premium cars from around the world.
           </p>
           <div className="flex justify-center text-center items-center py-0 xl:justify-start">

d3616a8b2345374958dceba7ccd089832cf83df5
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-19 18:01:55 +0100

    Create SubHero.js

---
+++ src/components/SubHero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PhoneMockup from '../images/PhoneMockup.png'
+
+function SubHero() {
+    return (
+        <div>
+      <h2>nam</h2>
+    </div>
+    )
+}
+
+export default SubHero

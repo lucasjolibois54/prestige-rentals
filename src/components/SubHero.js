eda93049bd21c93d4efad7a4901a154b249b04c9
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-19 18:31:21 +0100

    Update SubHero.js

--- src/components/SubHero.js
+++ src/components/SubHero.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import PhoneMockup from '../images/PhoneMockup.png'
+import phone2 from '../images/Phone2.png'
 
 function SubHero() {
     return (
-        <div>
-      <h2>Sub Hero</h2>
-    </div>
+      <div>
+      <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
+         <div className="w-full xl:w-9/12 grid lg:justify-items-end">
+          <img src={phone2} className="pt-16 w-full xl:w-9/12" alt="Phone Mockup" />
+      </div>
+      <div className="flex text-center flex-col w-9/12 px-3 justify-center xl:w25/10 xl:text-left xl:pl-10">
+          <h3 className="text-lg pt-10 text-gray-900 mb-3 font-light">
+          Convenient Interaction
+          </h3>
+          <h2 className="text-4xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans lg:mt-0 mt-4 text-dark-text py-0 mb-8">
+          Modern App
+              
+          </h2>
+          <p className="text-base  md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+          Don’t deny yourself the pleasure of driving the best premium cars from around the world here and now. Don’t deny yourself the pleasure of driving the best premium cars from around the world.
+          </p>
+          <div className="flex justify-center text-center items-center py-0 xl:justify-start">
+              <a href="#" >
+                  <button href="#" class="shadow-lg bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110">
+                   Download App
+                  </button>
+              </a>
+          </div>
+      </div>
+  </div>
+  </div>
     )
 }
 

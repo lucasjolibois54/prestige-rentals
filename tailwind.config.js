ae9816a588ac465f93d7227a587b0b1a6acc475a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-19 18:44:53 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#06080C',
+      }
+    },
   },
   variants: {
     extend: {},

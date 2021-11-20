3bd6d1090291f4c1deb0167ed8070b7bd9730f95
AeeUX <AndreasEriksen99@hotmail.com>
2021-11-20 16:06:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import SubHero from "./components/SubHero";
-import MainSection from "./components/MainSection";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import SubHero from './components/SubHero'
+import MainSection from './components/MainSection'
+import Features from './components/Features'
+import Footer from './components/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
-        <Nav/>
-       <Hero/>
-    <SubHero/>
-    <MainSection/>
-    <Features/>
-    <Footer/>
+    <div className="mx-auto justify-center max-w-screen-md">
+      <Nav />
+      <Hero />
+      <SubHero />
+      <MainSection />
+      <Features />
+      <Footer />
+    </div>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()

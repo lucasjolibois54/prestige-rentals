c5ddefc9eac5e242706c5f8777e45f04e6addabd
AeeUX <AndreasEriksen99@hotmail.com>
2021-11-20 16:47:10 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 function Hero() {
-    return (
-        <div>
-            <h2>Hero</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="text-8xl font-bold text-center mt-5">PRESTIGE</h2>
+      <h2 className="text-6xl text-center text-gray-500">CAR RENTAL</h2>
+      <Button className="outlined-btn" variant="outlined">
+        Learn more
+      </Button>
+      <Button className="contained-btn" variant="contained">
+        Download App
+      </Button>
+    </div>
+  )
 }
 
 export default Hero

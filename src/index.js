abe126f83ac34ad0d9402762f6f09afb05768f1d
AeeUX <AndreasEriksen99@hotmail.com>
2021-11-21 04:06:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-import Nav from './components/Nav'
-import Hero from './components/Hero'
-import SubHero from './components/SubHero'
-import MainSection from './components/MainSection'
-import Features from './components/Features'
-import Footer from './components/Footer'
-
-class App extends React.Component {
-  render() {
-    return (
-      <div id="container" ref={this.container} className="">
-        <div>
-          <Nav />
-        </div>
-        <div className="page first-page">
-          <Hero />
-        </div>
-        <div className="page second-page">
-          <MainSection />
-        </div>
-        <div className="page third-page">
-          <SubHero />
-        </div>
-        <div className="page fourth-page">
-          <Features />
-        </div>
-        <Footer />
-      </div>
-    )
-  }
-}
-
-/* ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
-    <div className="mx-auto justify-center max-w-screen-2xl">
-      <Nav />
-      <Hero />
-      <MainSection />
-      <SubHero />
-      <Features />
-      <Footer />
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
-) */
+)
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()

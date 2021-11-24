625226a9c56afc4f483ce57bc155571c6aecb6a7
AeeUX <AndreasEriksen99@hotmail.com>
2021-11-24 15:43:51 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  )
 }
 
 export default Footer

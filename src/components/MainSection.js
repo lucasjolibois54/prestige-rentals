92bd5d5c61768ee3fcd7008b67e18febe8cdf3b6
AeeUX <AndreasEriksen99@hotmail.com>
2021-11-24 16:33:07 +0100

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -20,74 +20,74 @@ function MainSection() {
         Featured Collection
       </h2>
       <section className="text-2xl flex justify-center mt-10">
-        <div className=" w-1/4 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className=" w-1/4 mx-5  rounded-3xl">
           <img
             id="fleet"
-            className="bg-center rounded-3xl w-full"
+            className="bg-center rounded-3xl w-full "
             src={
               'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
             }
             alt=""
           />
         </div>
-        <div className=" w-1/4 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className=" w-1/4 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637766767/Prestige-Rentals/carpixel.net-2021-mercedes-amg-gls-63-us-98210-hd_ft97q9.webp'
             }
             alt=""
           />
         </div>
-        <div className=" w-1/4 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className=" w-1/4 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637767211/Prestige-Rentals/carpixel.net-2018-mercedes-benz-cls-class-amg-line-76449-hd_zjzkmr.webp'
             }
             alt=""
           />
         </div>
       </section>
       <section className="text-2xl flex justify-center mt-8">
-        <div className="w-1/6 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className="w-1/6 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637767447/Prestige-Rentals/carpixel.net-2017-bentley-bentayga-falconry-by-mulliner-65836-hd_ui7a8z.webp'
             }
             alt=""
           />
         </div>
-        <div className="w-1/6 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className="w-1/6 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637767578/Prestige-Rentals/carpixel.net-2020-audi-s7-sportback-us-98958-hd_tkwyxq.webp'
             }
             alt=""
           />
         </div>
-        <div className="w-1/6 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className="w-1/6 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637767706/Prestige-Rentals/carpixel.net-2012-laferrari-prototype-73820-hd_ew017w.webp'
             }
             alt=""
           />
         </div>
-        <div className="w-1/6 mx-5 border-4 border-blue-500 rounded-3xl">
+        <div className="w-1/6 mx-5 rounded-3xl">
           <img
             id="fleet"
             className="bg-center rounded-3xl w-full"
             src={
-              'https://res.cloudinary.com/aeeux/image/upload/v1637761882/Prestige-Rentals/bmwX6-01_rf24bf.webp'
+              'https://res.cloudinary.com/aeeux/image/upload/v1637767945/Prestige-Rentals/carpixel.net-2016-audi-r8-coupe-au-49599-hd_ormuwi.webp'
             }
             alt=""
           />
